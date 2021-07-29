e52d4fda58f62c7dfd42096cb05d4247687f65d5
Sebastian <sarri070@uottawa.ca>
2021-07-28 22:14:24 -0400

    ye

--- script.js
+++ script.js
@@ -4,5 +4,23 @@ let c = canvas.getContext("2d");
 canvas.width = 768;
 canvas.height = 512;
 
-c.fillStyle = 'red';
-c.fillRect(120,120,120,120);
+var player, loop, tileMap;
+
+player = {
+    x: 512,
+    y: 0,
+    width: 50,
+    height: 50,
+    colour: 'red'
+}
+
+tileMap = {
+    image: new Image(),
+}
+
+loop = function() {
+
+
+window.requestAnimationFrame(loop);
+}
+window.requestAnimationFrame(loop);
