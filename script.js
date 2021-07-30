6d9f3a3fab4ad6a3c6f3be1bc1630fde6c39eb72
Sebastian <sarri070@uottawa.ca>
2021-07-30 09:24:17 -0400

    commit

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let canvas = document.getElementById("canvas");
-let c = canvas.getContext("2d");
+let canvas0 = document.getElementById("can0");
+let c = canvas0.getContext("2d");
+let canvas1 = document.getElementById("can1");
+let tm = canvas1.getContext("2d");
 
-canvas.width = 768;
-canvas.height = 512;
+canvas0.width = 1152;
+canvas0.height = 768;
+canvas1.width = 1152;
+canvas1.height = 768;
 
 var player, loop, tileMap;
 
@@ -16,7 +20,7 @@ player = {
 
 tileMap = {
     image: new Image(),
-    tile_size: 64,
+    tile_size: 96,
     map: [
         6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
         6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
@@ -27,16 +31,26 @@ tileMap = {
         6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6,
         6, 6, 6, 0, 0, 0, 0, 0, 6, 6, 6, 6
     ], 
-    tile_image_size: 16
+    tile_image_size: 16,
+    collums: 12,
+    rows: 8
 }
+
 
 
 tileMap.image.onload = function() {
-    tileMap.image.src = 'Tile Map.png';
-for (let i = 0; i < tileMap.map.length; i++) {
-console.log("ye");
+    for (let i = 0; i < tileMap.map.length; i++) {
+        var source_x = (tileMap.map[i] % tileMap.collums) * 16;
+        var source_y = Math.floor(tileMap.map[i] / tileMap.collums) * 16;
+        var destination_x = (i % tileMap.collums) * 96;
+        var destination_y = Math.floor(i / tileMap.collums) * 96;
+        tm.drawImage(tileMap.image, source_x, source_y, 16, 16, destination_x, destination_y, 96, 96);
+        console.log(tileMap.map[i])
+    }
 }
-}
+
+tileMap.image.src = 'Tile Map.png';
+tm.imageSmoothingEnabled = false;
 
 loop = function() {
 
